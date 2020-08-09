c3a9a1a81fe10f8fc34074d39b3edbdfa9c6250c
Anders Teglgaard <teglgaard@gmail.com>
2020-08-09 15:34:50 +0200

    Update MigrationServiceProvider.php

--- src/MigrationServiceProvider.php
+++ src/MigrationServiceProvider.php
@@ -186,7 +186,7 @@ class MigrationServiceProvider extends ServiceProvider {
     protected function registerMigrateMakeCommand()
     {
         $this->app->singleton('migration.neoeloquent.creator', function($app) {
-            return new MigrationCreator($app['files']);
+            return new MigrationCreator($app['files'],null);
         });
 
         $this->app->singleton('command.neoeloquent.migrate.make', function($app) {
